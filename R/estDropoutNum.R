95cebf5a6932caae683f38d572d8b958a688ce19
miaozhun <mz8@foxmail.com>
2018-08-23 15:23:15 +0800

    Update estDropoutNum.R

--- R/estDropoutNum.R
+++ R/estDropoutNum.R
@@ -1,14 +1,20 @@
 # Estimate dropout gene number in a cell
-estDropoutNum <- function(sample, depth){
-  sample <- as.numeric(sample)
-  sample <- ceiling(sample)
-  histCounts <- cbind(as.numeric(names(table(sample))), as.numeric(unname(table(sample))))
-  histCounts <- histCounts[-1,]
-  colnames(histCounts) <- c("j", "n_j")
+estDropoutNum <- function(sample = NULL, histCounts = NULL, depth = 100, return = "dropoutNum"){
+  library(preseqR)
+  if(!is.null(sample)){
+    sample <- as.numeric(sample)
+    sample <- ceiling(sample)
+    histCounts <- cbind(as.numeric(names(table(sample))), as.numeric(unname(table(sample))))
+    histCounts <- histCounts[-1,]
+    colnames(histCounts) <- c("j", "n_j")
+  }
   preseqR <- ztnb.rSAC(histCounts)
   geneNumPredict <- round(preseqR(depth))
-  dropoutNum <- geneNumPredict - sum(sample != 0)
-  return(dropoutNum)
+  dropoutNum <- geneNumPredict - sum(histCounts[,2])
+  if(return == "dropoutNum")
+    return(dropoutNum)
+  else
+    return(geneNumPredict)
 }
 
 
