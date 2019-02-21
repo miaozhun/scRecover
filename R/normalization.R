76313433a42f063df39cf08aa3fcb9a56e86bdd6
miaozhun <mz8@foxmail.com>
2019-02-21 23:05:41 +0800

    Update normalization.R

--- R/normalization.R
+++ R/normalization.R
@@ -24,7 +24,6 @@
 #' counts.norm <- normalization(counts = counts)
 #'
 #'
-#' @import stats
 #' @import doParallel
 #' @import foreach
 #' @import parallel
@@ -32,6 +31,7 @@
 #' @importFrom kernlab specc
 #' @importFrom rsvd rpca
 #' @importFrom graphics hist
+#' @importFrom stats complete.cases dgamma dnbinom dnorm prcomp quantile rgamma rnorm sd uniroot
 #' @importFrom utils read.csv read.table write.csv write.table
 #' @importFrom Matrix Matrix
 #' @importFrom MASS glm.nb fitdistr
