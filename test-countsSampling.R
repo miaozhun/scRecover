df9fae9ccb358a76910acea773160481b8d510e5
miaozhun <mz8@foxmail.com>
2019-04-03 18:14:51 +0800

    Add files via upload

---
+++ test-countsSampling.R
@@ -0,0 +1,31 @@
+context("test-countsSampling")
+
+
+test_that("Test that countsSampling samples correct fraction of reads", {
+  expect_equal(sum(countsSampling(c(10,20,30), fraction = 0)), 0)
+  expect_equal(sum(countsSampling(c(10,20,30), fraction = 0.5)), 30)
+  expect_equal(sum(countsSampling(c(10,20,30), fraction = 1)), 60)
+})
+
+
+test_that("Test that countsSampling's output length is correct", {
+  expect_equal(length(countsSampling(c(10), fraction = 0.5)), 1L)
+  expect_equal(length(countsSampling(c(10,20), fraction = 0.5)), 2L)
+  expect_equal(length(countsSampling(c(10,20,30), fraction = 0.5)), 3L)
+})
+
+
+test_that("Test that countsSampling's output is correct", {
+  set.seed(1)
+  expect_equal(countsSampling(c(10,20,30), fraction = 0.5), c(5,10,15))
+
+  set.seed(999)
+  expect_equal(countsSampling(c(10,20,30), fraction = 0.5), c(6,11,13))
+
+  expect_equal(countsSampling(c(10,20,30), fraction = 0), c(0,0,0))
+  expect_equal(countsSampling(c(10,20,30), fraction = 1), c(10,20,30))
+})
+
+
+
+
