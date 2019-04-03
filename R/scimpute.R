7bc1d6e9572b66c5924f1450fea464797c7d4697
miaozhun <mz8@foxmail.com>
2019-04-03 18:05:01 +0800

    Update scimpute.R

--- R/scimpute.R
+++ R/scimpute.R
@@ -2,9 +2,9 @@
 # scImpute original authors: Wei Vivian Li & Jingyi Jessica Li
 # scImpute reference: Li, Wei Vivian, and Jingyi Jessica Li. "An accurate and robust imputation method scImpute for single-cell RNA-seq data." Nature communications 9.1 (2018): 997.
 scimpute <-
-function (count_path, infile = "csv", outfile = "csv", type = "count", out_dir, labeled = FALSE, 
-          drop_thre = 0.5, Kcluster = NULL, labels = NULL, genelen = NULL, ncores = 5) 
-{   
+function (count_path, infile = "csv", outfile = "csv", type = "count", out_dir, labeled = FALSE,
+          drop_thre = 0.5, Kcluster = NULL, labels = NULL, genelen = NULL, ncores = 5)
+{
     if(labeled == TRUE & is.null(labels)){
       stop("'labels' must be specified when 'labeled = TRUE'!")
     }
@@ -13,33 +13,33 @@ function (count_path, infile = "csv", outfile = "csv", type = "count", out_dir, 
     }
     if(!(type %in% c("count", "TPM"))){ stop("expression values can be either 'count' or 'TPM'!") }
     if(type == "TPM" & is.null(genelen)){ stop("'genelen' must be specified when type = 'TPM'!") }
-  
+
     # print(drop_thre)
     print("reading in raw count matrix ...")
     dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
-    count_lnorm = read_count(filetype = infile, path = count_path, out_dir = out_dir, 
+    count_lnorm = read_count(filetype = infile, path = count_path, out_dir = out_dir,
                              type = type, genelen = genelen)
     print("reading finished!")
-    
-    if(labeled == TRUE){
+
+    if(labeled){
       if(length(labels) != ncol(count_lnorm)){
         stop("number of cells does not match number of labels !")
       }
     }
     genenames = rownames(count_lnorm)
     cellnames = colnames(count_lnorm)
-    
+
     print("imputation starts ...")
-    if (labeled == FALSE){
-      res_imp = imputation_model8(count = count_lnorm, labeled = FALSE, 
-                                  point = log10(1.01), drop_thre = drop_thre, 
-                                  Kcluster = Kcluster, 
+    if (!labeled){
+      res_imp = imputation_model8(count = count_lnorm, labeled = FALSE,
+                                  point = log10(1.01), drop_thre = drop_thre,
+                                  Kcluster = Kcluster,
                                   out_dir = out_dir, ncores = ncores)
     }else{
-      res_imp = imputation_wlabel_model8(count = count_lnorm, labeled = TRUE, 
-                                         cell_labels = labels, point = log10(1.01), 
-                                         drop_thre = drop_thre, 
-                                         Kcluster = NULL, out_dir = out_dir, 
+      res_imp = imputation_wlabel_model8(count = count_lnorm, labeled = TRUE,
+                                         cell_labels = labels, point = log10(1.01),
+                                         drop_thre = drop_thre,
+                                         Kcluster = NULL, out_dir = out_dir,
                                          ncores = ncores)
     }
     count_imp = res_imp$count_imp
